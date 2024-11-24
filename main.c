4211718be7e34de47d8b1993f92bc744df1268d6
ismardn <ismael.radouane@efrei.net>
2024-11-24 21:32:26 +0100

    petite modifications

--- main.c
+++ main.c
@@ -36,7 +36,8 @@ int main() {
 */
 
 int main() {
-    srand(3);
+
+    srand(time(NULL));
 
     t_map map = createMapFromFile("maps/example1.map");
 
@@ -50,8 +51,19 @@ int main() {
     }
     printf("\n");
 
-    int* moves_selected = moves_selections();
+    int* moves_selected;
     int phase_movements_number = EXECUTED_MOVES_NUMBER;
+
+    t_localisation start_loc;
+    start_loc.pos.x = 4;
+    start_loc.pos.y = 6;
+    start_loc.ori = NORTH;
+    
+    t_localisation current_loc = generate_start_loc(map.x_max, map.y_max);
+
+    printf("Position de depart generee :\nPosition Y : %d, Postion X : %d\nOrientation : %s\n\n", current_loc.pos.y, current_loc.pos.x, orientation_strings[current_loc.ori]);
+
+    moves_selected = moves_selections();
 
     printf("Mouvements selectionnes :\n");
     for (int i = 0; i < SELECTED_MOVES_NUMBER; i++) {
@@ -59,18 +71,11 @@ int main() {
     }
     printf("\n\n");
 
-    t_localisation start_loc;
-    start_loc.pos.x = 4;
-    start_loc.pos.y = 6;
-    start_loc.ori = NORTH;
-    
-    printf("Position de depart generee :\nPosition Y : %d, Postion X : %d\nOrientation : %s\n\n", start_loc.pos.y, start_loc.pos.x, orientation_strings[start_loc.ori]);
-
     double execution_time_tree = 0.0;
     clock_t begin_build = clock();
     
     t_tree tree;
-    buildTree(&tree, moves_selected, start_loc, map);
+    buildTree(&tree, moves_selected, current_loc, map);
 
     clock_t end_build = clock();
     execution_time_tree = ((double)(end_build-begin_build))/CLOCKS_PER_SEC;
@@ -85,20 +90,19 @@ int main() {
     double execution_time_best_moves = 0.0;
     clock_t begin_best_moves = clock();
     
-    t_localisation current_loc = start_loc;
     int reg_activated;
 
-    if (map.soils[start_loc.pos.y][start_loc.pos.x] == REG) {
+    if (map.soils[current_loc.pos.y][current_loc.pos.x] == REG) {
         reg_activated = 1;
     } else {
         reg_activated = 0;
     }
-
+    
     int* best_moves_executed = best_moves(&tree, phase_movements_number);
     
     clock_t end_best_moves = clock();
     execution_time_best_moves = ((double)(end_best_moves - begin_best_moves)) / CLOCKS_PER_SEC;
-    printf("Temps d'execution pour calculer le chemin de la racine vers la feuille : %.3f secondes\n", execution_time_best_moves);
+    printf("Temps d'execution pour calculer le chemin de la racine vers la feuille : %.3f secondes\n\n", execution_time_best_moves);
     
     if (best_moves_executed == NULL) {
         printf("La meilleure suite de mouvements n'a pas ete trouvee.\n");
@@ -118,6 +122,8 @@ int main() {
     if (reg_activated) {
         phase_movements_number = EXECUTED_MOVES_NUMBER - 1;
         reg_activated = 0;
+    } else {
+        phase_movements_number = EXECUTED_MOVES_NUMBER;
     }
 
     // Prochaine phase
