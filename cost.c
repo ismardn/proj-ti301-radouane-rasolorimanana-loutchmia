7394708d3fb4090141072d0c0fff03872c7b22d9
ismardn <ismael.radouane@efrei.net>
2024-11-26 17:40:12 +0100

    Modifications des meilleurs mouvements et des phases

--- cost.c
+++ cost.c
@@ -6,55 +6,49 @@
 #include "moves.h"
 
 /** 
- * @brief Trouve l'index du noeud ayant le coût le plus bas dans un tableau de noeuds.
- */
-int index_lowest_cost(t_node** nodes_tab, int tab_length) {
-    int minimal_cost = _soil_cost[4];
-    int minimal_index = 0;
-
-    for (int node_index = 0; node_index < tab_length; node_index++) {
-        if (nodes_tab[node_index]->cost == 0) {
-            minimal_cost = nodes_tab[node_index]->cost;
-            minimal_index = node_index;
-            break;
-        }
-        if (nodes_tab[node_index]->cost < minimal_cost) {
-            minimal_cost = nodes_tab[node_index]->cost;
-            minimal_index = node_index;
-        }
-    }
-
-    if (minimal_cost == _soil_cost[4]) {
-        return -1;
-    }
-
-    return minimal_index;
-}
-
-/** 
- * @brief Génère la file des mouvements optimaux en fonction des coûts dans l'arbre.
+ * @brief Trouve le chemin de mouvements pour la feuille avec le coût minimal.
+ * Parcourt l'arbre avec des boucles for et met à jour le tableau best_moves.
  */
 int* best_moves(t_tree* tree, int phase_movements_number) {
     int* best_moves_tab = (int*) malloc(phase_movements_number * sizeof(int));
     t_root* root_node = tree->root;
-    t_node** current_nodes_tab = root_node->child_nodes;
-    int index_lowest_cost_value = index_lowest_cost(current_nodes_tab, root_node->children_num);;
     
-    if (best_moves_tab[0] == -1) {
-        return NULL;
+    int current_moves_tab[5];
+    
+    for (int i = 0; i < phase_movements_number; i++) {
+        best_moves_tab[i] = COST_UNDEF;
     }
-    best_moves_tab[0] = current_nodes_tab[index_lowest_cost_value]->move_associated;
 
-    for (int nodes_tab_index = 1; nodes_tab_index < phase_movements_number; nodes_tab_index++) {
-        t_node* parent_node = current_nodes_tab[index_lowest_cost_value];
-        current_nodes_tab = parent_node->child_nodes;
+    int minimal_cost = COST_UNDEF;
+    
+    for (int i = 0; i < root_node->children_num; i++) {
+        for (int j = 0; j < root_node->child_nodes[i]->children_num; j++) {
+            for (int k = 0; k < root_node->child_nodes[i]->child_nodes[j]->children_num; k++) {
+                for (int l = 0; l < root_node->child_nodes[i]->child_nodes[j]->child_nodes[k]->children_num; l++) {
+                    for (int m = 0; m < root_node->child_nodes[i]->child_nodes[j]->child_nodes[k]->child_nodes[l]->children_num; m++) {
+                        t_node* leaf_node = root_node->child_nodes[i]->child_nodes[j]->child_nodes[k]->child_nodes[l]->child_nodes[m];
 
-        int index_lowest_cost_value = index_lowest_cost(current_nodes_tab, parent_node->children_num);
-        if (index_lowest_cost_value == -1) {
-            return NULL;
+                        if (leaf_node->cost != COST_UNDEF && leaf_node->cost < minimal_cost) {
+                            minimal_cost = leaf_node->cost;
+
+                            current_moves_tab[0] = root_node->child_nodes[i]->move_associated;
+                            current_moves_tab[1] = root_node->child_nodes[i]->child_nodes[j]->move_associated;
+                            current_moves_tab[2] = root_node->child_nodes[i]->child_nodes[j]->child_nodes[k]->move_associated;
+                            current_moves_tab[3] = root_node->child_nodes[i]->child_nodes[j]->child_nodes[k]->child_nodes[l]->move_associated;
+                            current_moves_tab[4] = leaf_node->move_associated;
+                        }
+                    }
+                }
+            }
         }
+    }
 
-        best_moves_tab[nodes_tab_index] = current_nodes_tab[index_lowest_cost_value]->move_associated;
+    for (int n = 0; n < phase_movements_number; n++) {
+        best_moves_tab[n] = current_moves_tab[n];
+    }
+
+    if (minimal_cost == COST_UNDEF) {
+        return NULL;
     }
 
     return best_moves_tab;
--- main.c
+++ main.c
@@ -37,7 +37,7 @@ int main() {
 
 int main() {
 
-    srand(time(NULL));
+    srand(100);
 
     t_map map = createMapFromFile("maps/example1.map");
 
@@ -59,74 +59,83 @@ int main() {
     start_loc.pos.y = 6;
     start_loc.ori = NORTH;
     
-    t_localisation current_loc = generate_start_loc(map.x_max, map.y_max);
+    t_localisation current_loc = start_loc;
 
     printf("Position de depart generee :\nPosition Y : %d, Postion X : %d\nOrientation : %s\n\n", current_loc.pos.y, current_loc.pos.x, orientation_strings[current_loc.ori]);
 
-    moves_selected = moves_selections();
+    int on_phase = 1;
+    int phase_number = 1;
 
-    printf("Mouvements selectionnes :\n");
-    for (int i = 0; i < SELECTED_MOVES_NUMBER; i++) {
-        printf("- %s\n", _moves[moves_selected[i]]);
-    }
-    printf("\n\n");
+    while (on_phase) {
+        printf("\n\nPhase %d :\n\n", phase_number);
 
-    double execution_time_tree = 0.0;
-    clock_t begin_build = clock();
-    
-    t_tree tree;
-    buildTree(&tree, moves_selected, current_loc, map);
+        moves_selected = moves_selections();
 
-    clock_t end_build = clock();
-    execution_time_tree = ((double)(end_build-begin_build))/CLOCKS_PER_SEC;
-    
-    if (tree.root != NULL) {
-        /*
-        display_full_tree(&tree);
-        //*/
-    }
-    printf("Temps d'execution de la phase de construction de l'arbre : %.3f secondes\n", execution_time_tree);
+        printf("Mouvements selectionnes :\n");
+        for (int i = 0; i < SELECTED_MOVES_NUMBER; i++) {
+            printf("- %s\n", _moves[moves_selected[i]]);
+        }
+        printf("\n\n");
 
-    double execution_time_best_moves = 0.0;
-    clock_t begin_best_moves = clock();
-    
-    int reg_activated;
+        double execution_time_tree = 0.0;
+        clock_t begin_build = clock();
+        
+        t_tree tree;
+        buildTree(&tree, moves_selected, current_loc, map);
 
-    if (map.soils[current_loc.pos.y][current_loc.pos.x] == REG) {
-        reg_activated = 1;
-    } else {
-        reg_activated = 0;
-    }
-    
-    int* best_moves_executed = best_moves(&tree, phase_movements_number);
-    
-    clock_t end_best_moves = clock();
-    execution_time_best_moves = ((double)(end_best_moves - begin_best_moves)) / CLOCKS_PER_SEC;
-    printf("Temps d'execution pour calculer le chemin de la racine vers la feuille : %.3f secondes\n\n", execution_time_best_moves);
-    
-    if (best_moves_executed == NULL) {
-        printf("La meilleure suite de mouvements n'a pas ete trouvee.\n");
-    } else {
-        printf("Liste des meilleurs mouvements :\n");
-        for (int moves_index = 0; moves_index < phase_movements_number; moves_index++) {
-            current_loc = move_exec(current_loc, best_moves_executed[moves_index], map, &reg_activated);
-            printf("%s\n", _moves[best_moves_executed[moves_index]]);
+        clock_t end_build = clock();
+        execution_time_tree = ((double)(end_build-begin_build))/CLOCKS_PER_SEC;
+        
+        if (tree.root != NULL) {
+            /*
+            display_full_tree(&tree);
+            //*/
+        }
+        printf("Temps d'execution de la phase de construction de l'arbre : %.3f secondes\n", execution_time_tree);
 
-            if (map.soils[current_loc.pos.x][current_loc.pos.y] == 0) {
-                printf("Le rover a retrouve la station ! Bien joue !");
-                break;
+        double execution_time_best_moves = 0.0;
+        clock_t begin_best_moves = clock();
+        
+        int reg_activated;
+
+        if (map.soils[current_loc.pos.y][current_loc.pos.x] == REG) {
+            reg_activated = 1;
+        } else {
+            reg_activated = 0;
+        }
+        
+        int* best_moves_executed = best_moves(&tree, phase_movements_number);
+        
+        clock_t end_best_moves = clock();
+        execution_time_best_moves = ((double)(end_best_moves - begin_best_moves)) / CLOCKS_PER_SEC;
+        printf("Temps d'execution pour calculer le chemin de la racine vers la feuille : %.3f secondes\n\n", execution_time_best_moves);
+        
+        if (best_moves_executed == NULL) {
+            printf("La meilleure suite de mouvements n'a pas ete trouvee.\n");
+            on_phase = 0;
+        } else {
+            printf("Liste des meilleurs mouvements :\n");
+            for (int moves_index = 0; moves_index < phase_movements_number; moves_index++) {
+                current_loc = move_exec(current_loc, best_moves_executed[moves_index], map, &reg_activated);
+                printf("%s\n", _moves[best_moves_executed[moves_index]]);
+
+                if (map.soils[current_loc.pos.x][current_loc.pos.y] == 0) {
+                    printf("Le rover a retrouve la station ! Bien joue !");
+                    on_phase = 0;
+                    break;
+                }
             }
         }
-    }
 
-    if (reg_activated) {
-        phase_movements_number = EXECUTED_MOVES_NUMBER - 1;
-        reg_activated = 0;
-    } else {
-        phase_movements_number = EXECUTED_MOVES_NUMBER;
-    }
+        if (reg_activated) {
+            phase_movements_number = EXECUTED_MOVES_NUMBER - 1;
+            reg_activated = 0;
+        } else {
+            phase_movements_number = EXECUTED_MOVES_NUMBER;
+        }
 
-    // Prochaine phase
+        phase_number++;
+    }
 
     return 0;
 }
